4441302dd77ccefca1ae023f5003c2466680ea1d
Lucas Della Bella <dellabella.lucas@gmail.com>
2017-09-25 13:10:56 -0400

    Add google stock scraping functionality

--- src/trump_stock/stock_data.clj
+++ src/trump_stock/stock_data.clj
@@ -10,3 +10,31 @@
                                                                 "symbol" symb,
                                                                 "interval" interval,
                                                                 "apikey" api-key}}))
+
+(def default-chrome-headers
+  {:headers {"Accept" "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8"
+             "Accept-Encoding" "gzip, deflate, br"
+             "Accept-Language" "en-US,zh-CN;q=0.8,zh;q=0.6,en;q=0.4"
+             "User-Agent" "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.91 Safari/537.36"}})
+
+(defn space-to-plus [orig-string]
+  "Converts all spaces in a string to pluses"
+  (apply str (map #(if (= % \space) \+ %) orig-string)))
+
+(defn build-search-string [entity]
+  "URL encodes entity and then searches"
+  (->> entity space-to-plus (str "https://www.google.com/search?q=stock+")))
+
+(defn find-stock-value [http-resp]
+  (->> http-resp
+       :body
+       (re-find #"data-value=\"\d+\.?\d*") ;grabs data value segment
+       (re-find #"\d+\.?\d*") ;grabs just the number from the expression
+       Double/parseDouble))
+
+(defn get-stock-ticker-and-cost [entity]
+  "If entity is a public company, return that company's ticker and share value"
+  (-> entity
+      build-search-string
+      (http/get default-chrome-headers)
+      find-stock-value))
